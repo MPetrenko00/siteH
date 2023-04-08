9c199e894effe5ba907a5634d0f9d2cc9b80c5f4
m.petrenko <mandarinkashusha@gmail.com>
2023-04-08 22:50:31 +0300

    svg_2

--- app/js/main.js
+++ app/js/main.js
@@ -45,9 +45,3 @@ const swiper = new Swiper('.swiper', {
 		clickable: true,
   },
 });
-
-var image = document.getElementsByClassName('thumbnail');
-new simpleParallax(image, {
-	delay: .6,
-	transition: 'cubic-bezier(0,0,0,1)'
-});
